b9b8b2d979e1a3a5ab4bad75ba89de059df93706
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-07-13 22:01:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,6 +15,6 @@ function init() {
   // your code here
   var event = document.body;
   event.addEventListener('keydown', function(event) {
-    event.key === 
+    event.key === codes[0] 
   })
 }
