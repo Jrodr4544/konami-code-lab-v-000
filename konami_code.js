3900607f429a8a0d4e6dfc8ccb7de42314506f9a
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-07-13 21:49:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ const codes = [
 function init() {
   // your code here
   var event = document.body;
-  event.addEventListener('', function(event) {
+  event.addEventListener('keydown', function(event) {
     alert('I was clicked!')
   })
 }
